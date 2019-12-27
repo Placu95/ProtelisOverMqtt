a6c6f5e8ea0cfb2f174aa373210971ee8acd7e64
Placu <andrea.placuzzi@studio.unibo.it>
2019-12-27 13:49:31 +0100

    add execution context with mqtt support

---
+++ src/main/kotlin/it.unibo.protelisovermqtt/executioncontext/MQTTPositionedExecutionContext.kt
@@ -0,0 +1,22 @@
+package it.unibo.protelisovermqtt.executioncontext
+
+import it.unibo.protelisovermqtt.model.LatLongPosition
+import it.unibo.mqttclientwrapper.api.MqttClientBasicApi
+import it.unibo.mqttclientwrapper.api.MqttMessageType
+import org.protelis.lang.datatype.impl.StringUID
+import org.protelis.vm.ExecutionEnvironment
+import org.protelis.vm.NetworkManager
+import org.protelis.vm.impl.SimpleExecutionEnvironment
+
+abstract class MQTTPositionedExecutionContext(
+    _deviceUID: StringUID,
+    nodePosition: LatLongPosition,
+    protected val mqttClient: MqttClientBasicApi,
+    val netmgr: NetworkManager,
+    val randomSeed: Int = 1,
+    protected val execEnvironment: ExecutionEnvironment = SimpleExecutionEnvironment()
+) : PositionedExecutionContext(_deviceUID, nodePosition, netmgr, randomSeed, execEnvironment) {
+
+    protected fun <T: MqttMessageType> subscribeTopic(topic: String, type: Class<T>, consumer: (topic: String, message: T) -> Unit) =
+        mqttClient.subscribe(this, topic, type, consumer)
+}
