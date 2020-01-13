dfd18039c084819df1ca45128a06d6c3f64e666c
Placu <andrea.placuzzi@studio.unibo.it>
2020-01-13 11:10:26 +0100

    add possibility to specify two mqttClient, one for EC and one for net manager

--- src/main/kotlin/it.unibo.protelisovermqtt/node/GenericNode.kt
+++ src/main/kotlin/it.unibo.protelisovermqtt/node/GenericNode.kt
@@ -1,7 +1,7 @@
 package it.unibo.protelisovermqtt.node
 
-import it.unibo.protelisovermqtt.model.LatLongPosition
 import it.unibo.mqttclientwrapper.api.MqttClientBasicApi
+import it.unibo.protelisovermqtt.model.LatLongPosition
 import it.unibo.protelisovermqtt.networkmanager.MQTTNetMgrWithMQTTNeighborhoodMgr
 import org.protelis.lang.datatype.impl.StringUID
 import org.protelis.vm.ExecutionContext
@@ -14,17 +14,27 @@ abstract class GenericNode(
     val sleepTime: Long,
     val deviceUID: StringUID,
     val applicationUID: String,
-    val mqttClient: MqttClientBasicApi,
+    netManagerMqttClient: MqttClientBasicApi,
+    val execContextMqttClient: MqttClientBasicApi,
     initialPosition: LatLongPosition,
     neighbors: Set<StringUID> = emptySet()
 ) {
+    constructor(
+        protelisProgram: ProtelisProgram,
+        sleepTime: Long,
+        deviceUID: StringUID,
+        applicationUID: String,
+        mqttClient: MqttClientBasicApi,
+        initialPosition: LatLongPosition,
+        neighbors: Set<StringUID> = emptySet()
+    ) : this(protelisProgram, sleepTime, deviceUID, applicationUID, mqttClient, mqttClient, initialPosition, neighbors)
 
     var position: LatLongPosition by observable(initialPosition) {
         _, old, new -> if (old != new) networkManager.changePosition(new)
     }
 
     protected val networkManager =
-        MQTTNetMgrWithMQTTNeighborhoodMgr(deviceUID, mqttClient, applicationUID, position, neighbors)
+        MQTTNetMgrWithMQTTNeighborhoodMgr(deviceUID, netManagerMqttClient, applicationUID, position, neighbors)
     protected val executionContext by lazy { createContext() }
     private val protelisVM by lazy { ProtelisVM(protelisProgram, executionContext) }
 
